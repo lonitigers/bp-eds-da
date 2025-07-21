7f2203ddcbf99274f14534613f3555850aeba66f
nilo.l.luansing.jr <nilo.luansing@gmail.com>
2025-07-21 18:21:56 +0800

    Update wans.js

--- scripts/wans.js
+++ scripts/wans.js
@@ -1,7 +1,20 @@
 console.log('v1');
 console.log('this is my production branch');
-function getCurrentDomain() {
-  return window.location.hostname;
+if(window.location.hostname == 'da.live/')
+{
+  console.log('you are running on da.live');
+}
+else if(window.location.hostname == 'main--bp-eds-da--lonitigers.aem.page')
+{
+  console.log('you are running on preview');
+}
+else if(window.location.hostname == 'main--bp-eds-da--lonitigers.aem.live')
+{
+  console.log('you are running on publish');
+}
+else 
+{
+  console.log('unable to identify domain');
 }
 
 // Usage:
